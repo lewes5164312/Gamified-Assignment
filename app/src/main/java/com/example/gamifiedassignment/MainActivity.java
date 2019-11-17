733ef2eeb4e2333e292c0d08fd2ad0b548703b65
Lewes <z5164312@student.unsw.edu.au>
2019-11-17 17:12:35 +1100

    3 fragments

--- app/src/main/java/com/example/gamifiedassignment/MainActivity.java
+++ app/src/main/java/com/example/gamifiedassignment/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.gamifiedassignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment fragment = new HomeFragment();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frag_slot, fragment);
+        fragmentTransaction.commit();
+
+        BottomNavigationView navMenu = findViewById(R.id.bottomNavBar);
+
+        navMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.nav_home) {
+                    Fragment fragment = new HomeFragment();
+                    swapFragment(fragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_learn) {
+                    Fragment fragment = new LearnFragment();
+                    swapFragment(fragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_quiz) {
+                    Fragment fragment = new QuizFragment();
+                    swapFragment(fragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    //Swapfragment method From NYT demo
+    private void swapFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frag_slot, fragment);
+        fragmentTransaction.commit();
     }
 }
