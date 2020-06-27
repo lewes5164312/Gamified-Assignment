8074dde2b3a1056e99b2113e95e5292aa67e6d10
lewes <55799888+lewes5164312@users.noreply.github.com>
2020-06-27 21:11:50 +1000

    firebase connection & login xml

--- app/src/main/java/com/example/GeoQuizzer/MainActivity.java
+++ app/src/main/java/com/example/GeoQuizzer/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.GeoQuizzer;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
